a7b2942cdc45a858e65a012544d47f7655f27494
Filip Kin <filip@kinmails.com>
2018-03-29 09:33:40 -0400

    php lol

--- filipkin.com/rm-sheet.php
+++ filipkin.com/rm-sheet.php
@@ -14,7 +14,7 @@ if ($status[1] == []) {
   fwrite($argfile, json_encode($jsonout, JSON_UNESCAPED_SLASHES));
   $output = shell_exec('python3 rm-sheet.py');
   $cleanout = str_replace("\n", "", $output);
-  $status[0] = json_decode($cleanout).status;
+  $status[0] = json_decode($cleanout)->status;
 }
 echo '{"id": "'.$id.'", "status": '.json_encode($status, JSON_UNESCAPED_SLASHES).'}';
 ?>
