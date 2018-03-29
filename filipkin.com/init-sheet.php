7790e48fc3a699b28cd28f6c4512a09be5552f79
Filip Kin <filip@kinmails.com>
2018-03-29 09:32:17 -0400

    php lol

--- filipkin.com/init-sheet.php
+++ filipkin.com/init-sheet.php
@@ -35,8 +35,8 @@ if ($status[1] == []) {
   fwrite($argfile, json_encode($jsonout, JSON_UNESCAPED_SLASHES));
   $output = shell_exec('python3 init-sheet.py');
   $cleanout = str_replace("\n", "", $output);
-  $status[0] = json_decode($cleanout).status;
-  array_push($status[1], json_decode($cleanout).url);
+  $status[0] = json_decode($cleanout)->status;
+  array_push($status[1], json_decode($cleanout)->url);
 }
 echo '{"email": "'.$email.'", "status": '.json_encode($status, JSON_UNESCAPED_SLASHES).'}';
 ?>
