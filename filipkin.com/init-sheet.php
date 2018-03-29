23f4cf1f891ce43e342b73561a4128fe9221bcb7
Filip Kin <filip@kinmails.com>
2018-03-29 09:44:29 -0400

    removed output double list

--- filipkin.com/init-sheet.php
+++ filipkin.com/init-sheet.php
@@ -36,7 +36,7 @@ if ($status[1] == []) {
   $output = shell_exec('python3 init-sheet.py');
   $cleanout = str_replace("\n", "", $output);
   $status[0] = json_decode($cleanout)->status;
-  array_push($status[1], json_decode($cleanout)->url);
+  $status[1] = json_decode($cleanout)->url;
 }
 echo '{"email": "'.$email.'", "status": '.json_encode($status, JSON_UNESCAPED_SLASHES).'}';
 ?>
