af90d9484d17bf7d45e96a8e94c8051b12943e23
filip9696 <filip@kinmails.com>
2018-04-06 18:42:23 -0400

    chrome.history.search() is synced so need to only init per account

--- install.js
+++ install.js
@@ -19,68 +19,82 @@ chrome.runtime.onInstalled.addListener(function() {
 
 	//Get history from before install
 	chrome.storage.local.get("history", function(data) {
-		if(!data.hasOwnProperty("history") && device !== "Unknown" && device !== "") {
-			var query = { 
-				text: "",
-				startTime: 1,
-				maxResults: 1000000
-			}
-			chrome.history.search(query, function(results) {
-				var out = [];
-				var lasttime = 10000000000000.00;
-				results.forEach(function(obj) {
-					console.debug(obj);
-					var direct = false;
-					if (obj.typedCount != 0) direct = true;
-					var timestamp = obj.lastVisitTime;
-					if (obj.visitCount != 1) {
-						timestamp = lasttime;
-					} else {
-						lasttime = timestamp;
-					}
-					var t = new Date(timestamp);
-					var tzTime = (t.getHours())+":"+(t.getMinutes())+":"+(t.getSeconds())+" "+(t.getMonth()+1)+"/"+(t.getDate())+"/"+(t.getFullYear());
-					var outobj = {
-						"id": obj.id,
-						"title": obj.title,
-						"url": obj.url,
-						"direct": direct,
-						"visits": obj.visitCount,
-						"time": tzTime
-					};
-					out.push(outobj);
-				});
-				console.log(out);
-				chrome.identity.getProfileUserInfo(function(id) {
-					var xhr = new XMLHttpRequest();
-					var json = JSON.stringify(out);
-					xhr.open("POST", 'https://filipkin.com/whib/init-sheet.php?email=' + encodeURIComponent(id.email) + "&device=" + encodeURIComponent(device), true);
-					xhr.onreadystatechange = function() {
-						if(xhr.readyState == 4) {
-							var resp = JSON.parse(xhr.responseText);
-							if (resp.status[0] == "exists") {
-								console.log("History storage already initiated, id = " + resp.status[1])
-								chrome.storage.local.set({history: resp.status[1]}, function() {
-
-								});
-							} else if (resp.status[0] == "created") {
-								console.log("History storage initiated, id = " + resp.status[1])
-								chrome.storage.local.set({history: resp.status[1]}, function() {
-								});
-							} else {
-								console.log("History init failed: " + JSON.stringify(resp.status))
+		if(!data.hasOwnProperty("history")) {
+			chrome.identity.getProfileUserInfo(function(id) {
+				var xhr = new XMLHttpRequest();
+				var json = JSON.stringify(out);
+				xhr.open("POST", 'https://filipkin.com/whib/get-sheet.php?email=' + encodeURIComponent(id.email), true);
+				xhr.onreadystatechange = function() {
+					if(xhr.readyState == 4) {
+						var resp = JSON.parse(xhr.responseText);
+						if (resp.status[0] == "exists") {
+							console.log("History storage already initiated, id = " + resp.status[1])
+							chrome.storage.local.set({history: resp.status[1]}, function() {
+								console.log("Recorded sheet id of history");
+							});
+						} else {
+							var query = { 
+								text: "",
+								startTime: 1,
+								maxResults: 1000000
 							}
+							chrome.history.search(query, function(results) {
+								var out = [];
+								var lasttime = 10000000000000.00;
+								results.forEach(function(obj) {
+									console.debug(obj);
+									var direct = false;
+									if (obj.typedCount != 0) direct = true;
+									var timestamp = obj.lastVisitTime;
+									if (obj.visitCount != 1) {
+										timestamp = lasttime;
+									} else {
+										lasttime = timestamp;
+									}
+									var t = new Date(timestamp);
+									var tzTime = (t.getHours())+":"+(t.getMinutes())+":"+(t.getSeconds())+" "+(t.getMonth()+1)+"/"+(t.getDate())+"/"+(t.getFullYear());
+									var outobj = {
+										"id": obj.id,
+										"title": obj.title,
+										"url": obj.url,
+										"direct": direct,
+										"visits": obj.visitCount,
+										"time": tzTime
+									};
+									out.push(outobj);
+								});
+								console.log(out);
+								chrome.identity.getProfileUserInfo(function(id) {
+									var xhr = new XMLHttpRequest();
+									var json = JSON.stringify(out);
+									xhr.open("POST", 'https://filipkin.com/whib/init-sheet.php?device=Synchronized&email=' + encodeURIComponent(id.email), true);
+									xhr.onreadystatechange = function() {
+										if(xhr.readyState == 4) {
+											var resp = JSON.parse(xhr.responseText);
+											if (resp.status[0] == "exists") {
+												console.log("History storage already initiated, id = " + resp.status[1])
+												chrome.storage.local.set({history: resp.status[1]}, function() {
+				
+												});
+											} else if (resp.status[0] == "created") {
+												console.log("History storage initiated, id = " + resp.status[1])
+												chrome.storage.local.set({history: resp.status[1]}, function() {
+												});
+											} else {
+												console.log("History init failed: " + JSON.stringify(resp.status))
+											}
+										}
+									}
+									xhr.send(json);
+								});
+							});
 						}
 					}
-					xhr.send(json);
-				});
+				}
+				xhr.send(json);
 			});
 		} else {
-			if (device == "Unknown" || device == "") {
-				console.log("Device name not changed from default, will not init history");
-			} else {
-				console.log("History storage already initiated, id = " + data.history);
-			}
+			console.log("History storage already initiated, id = " + data.history);
 		}
 	});
 
